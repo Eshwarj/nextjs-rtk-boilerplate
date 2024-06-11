2286bf17b05681088167fb6386d52d8989b6c6ec
Eshwarj <eshwarrj@gmail.com>
2024-06-11 23:22:52 +0530

    prettier and ES lint Config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
+  webpack(config) {
+    config.externals.push("pino-pretty", "lokijs", "encoding")
 
-export default nextConfig;
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    })
+
+    return config
+  },
+}
+
+export default nextConfig
